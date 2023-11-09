893e56fa4fcd7f74a268c064e0582d0e20bb45d6
Jammeren2 <52398081+Jammeren2@users.noreply.github.com>
2023-11-09 17:04:02 +0700

    human.py

--- human.py
+++ human.py
@@ -1,29 +1,47 @@
-from tkinter import Tk, Canvas, ARC, SE, W
+from tkinter import *
 
 class Human() :
-    def __init__ (self, canvas):
+    def __init__ (self, canvas, x, y, name):
         self.canvas = canvas
-        
+        self.x = x
+        self.y = y
+        self.name = name
     def display(self):
         self.__drawHead()
         self.__drawBody()
         self.__TEXT()
         
     def __drawHead(self) :
-        self.canvas.create_oval(20, 400, 80, 460, width=2)
-        self.canvas.create_arc(30, 420, 70, 450, start=0, extent=-180, style=ARC, outline="red", width=3)
-        self.canvas.create_oval(35, 421, 43, 429, width=0, fill="blue")
-        self.canvas.create_oval(65, 420, 57, 428, width=0, fill="blue")
-    def __drawBody(self) :
-        self.canvas.create_line(50, 460, 50, 550, width=2)
-        
-        self.canvas.create_line(100, 600, 50, 550, width=2)
-        self.canvas.create_line(0, 600, 50, 550, width=2) 
-        
-        self.canvas.create_line(100, 520, 50, 480, width=2)
-        self.canvas.create_line(0, 520, 50, 480, width=2)
+        self.canvas.create_oval(self.x, self.y-170, self.x+100, self.y-270, width=2)
 
+        
+        self.canvas.create_arc(self.x+24, self.y-200, self.x+69, self.y-230, start=0, extent=-180, style=ARC, outline="blue", width=3)
+        
+        self.canvas.create_oval(self.x+60, self.y-220, self.x+75, self.y-235, width=0, fill="red")# ПРАВОЕ ЯЙЦО
+        
+        self.canvas.create_oval(self.x+15, self.y-220, self.x+30, self.y-235, width=0, fill="red")# ЛЕВОЕ ЯЙЦО
+        
+        
+        
+
+    def __drawBody(self) :
+        self.canvas.create_line(self.x, self.y, self.x+50, self.y-50, self.x+50, self.y-170, width=2)# тело
+ 
+        self.canvas.create_line(self.x, self.y-110, self.x+50, self.y-150, self.x+100, self.y-110, width=2)# руки
+
+        self.canvas.create_line(self.x, self.y, self.x+50, self.y-50, self.x+100, self.y, width=2)# ноги
+        
+
+        
+
+        
     def __TEXT(self) : 
-        self.canvas.create_text(50, 370, 
-              text="Боба",
+        self.canvas.create_text(self.x+50, self.y-300, 
+              text=self.name,
               justify=CENTER, font="Verdana 14")
+              
+        
+        
+        
+        
+        
